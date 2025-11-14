92e56434e5de65f87f27a1fde405319958beeb71
Dang Hoang <danghmh1205@gmail.com>
2025-11-14 19:42:07 +0700

    initial project setup

---
+++ Expenses-Tracking/Expenses_TrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expenses_TrackingApp.swift
+//  Expenses-Tracking
+//
+//  Created by Hoàng Minh Hải Đăng on 14/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Expenses_TrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
